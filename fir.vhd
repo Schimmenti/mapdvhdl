b3fe1127aff10186734322a7070aef8af98565ca
Schimmenti <vinci95s@gmail.com>
2018-12-17 16:39:35 +0100

    vedo gente

--- fir.vhd
+++ fir.vhd
@@ -17,9 +17,9 @@ end fir;
 --examples for coefficents
 --we use fixed point binary point with 11 bits for fractional part and 4 for integer part and 1 for the sign
 --max positive number 15.99951171875
---0.19335315 approx 0.001100010111 -> 0|0000|00110001011 
---0.20330353 approx 0.001101000000 -> 0|0000|00110100000
---0.20668665 approx 0.001101001110 -> 0|0000|00110100111
+--0.19335315 approx 0.001100010111 -> 0|0000|00110001011 -> after a Q=11 leftshift 0x18B
+--0.20330353 approx 0.001101000000 -> 0|0000|00110100000 -> after a Q=11 leftshift 0x1A0
+--0.20668665 approx 0.001101001110 -> 0|0000|00110100111 -> after a Q=11 leftshift 0x1A7
 
 -- -2.7896 approx -0010.11001010001 -> abs(-2.7896 ) approx 0|0010|11001010001) -> 1|1101|00110101110 + 1 -> 1|1101|00110101111  
 architecture rtl of fir is
@@ -27,6 +27,8 @@ architecture rtl of fir is
 signal y01,y12,y23,y34 : std_logic_vector(N-1 downto 0)  := (others => '0');
 signal x_sum_part : std_logic_vector(2*N-1 downto 0);
 signal x_sum : std_logic_vector(N-1 downto 0);
+signal m0,m1,m2,m3,m4 : signed(2*N-1 downto 0):= (others => '0');
+signal sgn : std_logic := '0';
 
 component fflop
  Port (clk  :   in  std_logic;
@@ -44,18 +46,28 @@ f5 : fflop port map (clk => clk, rst => rst, a_in => x_sum, ff_out  => y_out);
 
 
 
-p_fir : process(clk,rst)
-variable sgn : std_logic;
-    begin
-    if rst = '1' then
-    x_sum <= (others => '0');
-    elsif rising_edge(clk) then
-    x_sum_part <= std_logic_vector(shift_right(c0*signed(x_in)+c1*signed(y01)+c2*signed(y12)+c3*signed(y23)+c4*signed(y34),Q));
+m0 <= c0*signed(x_in);
+m1 <= c1*signed(y01);
+m2 <= c2*signed(y12);
+m3 <= c3*signed(y23);
+m4 <= c4*signed(y34);
+x_sum_part <= std_logic_vector(shift_right(m0+m1+m2+m3+m4, Q));
+sgn <= x_sum_part(2*N-1);
+x_sum <= '0' & x_sum_part(N-2 downto 0);
+
+
+--p_fir : process(clk,rst)
+--variable sgn : std_logic;
+--    begin
+ --   if rst = '1' then
+   -- x_sum <= (others => '0');
+    --elsif rising_edge(clk) then
+    --x_sum_part <= std_logic_vector(shift_right(c0*signed(x_in)+c1*signed(y01)+c2*signed(y12)+c3*signed(y23)+c4*signed(y34),Q));
      --x_sum_part <= std_logic_vector(c0*signed(x_in)+c1*signed(y01)+c2*signed(y12)+c3*signed(y23)+c4*signed(y34));
-    sgn := x_sum_part(2*N-1);
-     x_sum <= sgn & x_sum_part(N-2 downto 0);
-    end if;
-end process;
+   -- sgn := x_sum_part(2*N-1);
+    --x_sum <= '0' & x_sum_part(N-2 downto 0);
+    --end if;
+--end process;
 
 
 end rtl;
--- tb_fir.vhd
+++ tb_fir.vhd
@@ -1,4 +1,5 @@
 library IEEE;
+
 use IEEE.STD_LOGIC_1164.ALL;
 
 USE ieee.std_logic_unsigned.all;
@@ -6,6 +7,7 @@ USE IEEE.NUMERIC_STD.ALL;
 
 USE std.textio.ALL;
 USE IEEE.STD_LOGIC_TEXTIO.ALL;
+
 
 --USE IEEE.STD_LOGIC_ARITH.ALL;
 
@@ -27,30 +29,44 @@ component fir
 end component;
 
 signal clk : std_logic :='0';
-signal rst : std_logic :='0';
-signal xin : std_logic_vector(N-1 downto 0) := std_logic_vector(to_signed(1,N));
+signal rst : std_logic :='1';
+signal xin : std_logic_vector(N-1 downto 0) := std_logic_vector(to_signed(1000,N));
 signal yout : std_logic_vector(N-1 downto 0);
 signal cnt : unsigned(N-1 downto 0) := (others => '0');
 signal c0,c1,c2,c3,c4 : signed(N-1 downto 0);
 
 begin
-c0 <= to_signed(1,N);
-c1 <= to_signed(-2,N);
-c2 <= to_signed(1,N);
-c3 <= to_signed(-2,N);
-c4 <= to_signed(1,N);
+c0 <= to_signed(16#18B#,N);
+--c0 <= to_signed(193353*(2**10)/1000000,N);
+c1 <= to_signed(16#1A0#,N);
+c2 <= to_signed(16#1A7#,N);
+c3 <= to_signed(16#1A0#,N);
+c4 <= to_signed(16#18B#,N);
 fir0 : fir port map (clk => clk, rst => rst, x_in => xin,y_out => yout, c0=>c0,c1=>c1,c2=>c2,c3=>c3,c4=>c4);
-sim_fir : process(clk,rst) is
+
+fir_clk : process(clk,rst) is
     begin
-    if cnt = 0 then
-    rst <= '1';
-    rst <= '0' after 50 ns;
-    cnt <= cnt +1;
+    if rst = '1' then
+        rst <= '0';
     else
-    clk <= not clk after 50 ns;
-    cnt <= cnt + 1;
+        clk <= not clk after 50 ns;
+        cnt <= cnt + 1;
     end if;
 end process;
+
+
+--sim_fir : process(clk,rst) is
+ --   begin
+  --  if cnt = 0 then
+   -- rst <= '1';
+   -- rst <= '0' after 50 ns;
+    --cnt <= cnt +1;
+    --else
+    --clk <= not clk after 50 ns;
+    --cnt <= cnt + 1;
+    --xin <= std_logic_vector(cnt);
+    --end if;
+--end process;
 
 
 end Behavioral;
